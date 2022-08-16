f68fc17315c4c32ac9f967739bfd639edb55fbfb
Grazziano Fagundes <grazzianofagundes@gmail.com>
2022-08-16 11:07:13 -0300

    Feat: Parâmetros do Jogo

---
+++ FlappyBird.py
@@ -0,0 +1,18 @@
+import pygame
+import os
+import random
+
+SCREEN_WIDTH = 500
+SCREEN_HEIGHT = 800
+
+PIPE_IMAGE = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "pipe.png")))
+FLOOR_IMAGE = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "base.png")))
+BACKGROUND_IMAGE = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bg.png")))
+BIRD_IMAGES = [
+    pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bird1.png"))),
+    pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bird2.png"))),
+    pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bird3.png"))),
+]
+
+pygame.font.init()
+FONT_POINTS = pygame.font.SysFont("arial", 50)
