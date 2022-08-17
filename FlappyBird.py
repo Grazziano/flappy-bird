e759191a7c06536c113474f1a708bd3c7da8c589
Grazziano Fagundes <grazzianofagundes@gmail.com>
2022-08-17 11:13:10 -0300

    Feat: Funções de Pular e Mover o Pássaro

--- FlappyBird.py
+++ FlappyBird.py
@@ -35,6 +35,32 @@ class Bird:
         self.image_count = 0
         self.image = IMGS[0]
 
+    def jump(self):
+        self.speed = -10.5
+        self.time = 0
+        self.height = self.y
+
+    def move(self):
+        # calcular o deslocamento
+        self.time += 1
+        displacement = 1.5 * (self.time ** 2) + self.speed * self.time
+
+        # restringir o deslocamento
+        if displacement > 16:
+            displacement = 16
+        elif displacement < 0:
+            displacement -= 2
+
+        self.y += displacement
+
+        # o angulo do pássaro
+        if displacement < 0 or self.y < (self.height + 50):
+            if self.angle < self.MAX_ROTATION:
+                self.angle = self.MAX_ROTATION
+        else:
+            if self.angle > -90:
+                self.angle -= self.ROTATION_SPEED
+
 
 class Pipe:
     pass
